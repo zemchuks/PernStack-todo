a556c0beabcce14c119327ce5eacb6b9318b1a34
Bruno Austin <brunoaugustine2@gmail.com>
2021-03-04 12:38:54 +0100

    ready for deployment

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ app.use(bodyParser.json())
 app.use(cors());
 
 app.use(express.static('client/build'))
+
 //Register and Login Route
 app.use('/auth', require('./routes/auth'))
 // Dashboard Route
@@ -26,8 +27,14 @@ app.get('/', (req, res) => {
 if(process.env.NODE_ENV === 'production') {
     //set static folder
     app.use(express.static('client/build'))
-    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html' )))
+    app.get('*', (req, res) => { 
+        res.sendFile(path.join(__dirname, "client/build/index.html" ))
+    })  
 }
+
+app.all('*', (req, res, next) => {
+    res.status(404).json({ error: 'The Route you are requesting for does not exist' });
+})
 
 const PORT = process.env.PORT
 
